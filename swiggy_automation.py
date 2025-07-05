23ad668b96e994384c8876afd1aca62bccc5905a
Dinesh_Meruga <143542812+Dinesh80744@users.noreply.github.com>
2025-07-05 19:22:36 +0530

    Update swiggy_automation.py

--- swiggy_automation.py
+++ swiggy_automation.py
@@ -30,7 +30,7 @@ try:
     phone_input = WebDriverWait(driver, 10).until(
         EC.presence_of_element_located((By.XPATH, "//input[@type='tel' or @id='mobile']"))
     )
-    phone_input.send_keys("7093773054")  # 9030366809  8688426259  9032208078 8125476289
+    phone_input.send_keys("Phone_number")
     login_after_phone = WebDriverWait(driver, 10).until(
         EC.element_to_be_clickable((By.XPATH, "//*[@id='overlay-sidebar-root']/div/div/div[2]/div/div/div/form/div[2]/a"))
     )
